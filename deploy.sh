ac364a678f7195dc8909aa5b782c6f1fccc3aea7
Devendra Maharjan <devendram@gmail.com>
2025-03-08 18:42:16 +0545

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -32,122 +32,123 @@ file(){
 
 # function to get configuration values
 config(){
-        cat $deploy_dir/deploy.conf | grep "^[^#].*" | grep "$1" | sed "s/.*='\(.*\)'/\1/g"
+	cat $deploy_dir/deploy.conf | grep "^[^#].*" | grep "$1" | sed "s/.*='\(.*\)'/\1/g"
 }
 
 # function to validate fields 
 validate(){
-        validation='1'
-        IFS=$'\n'
-        for field in $(echo $1 | sed 's/,/\n/g')
-        do
-                varname=$(echo $field | grep -o  ".*=>" | grep -o "\".*\"" | grep -o "[^\"]*")
-                value=$(echo $field | grep -o  "=>.*" | grep -o "\".*\"" | grep -o "[^\"]*" | sed 's/^\s*\(.*\)\s*$/\1/g')
-                if [ "$value" = "" ];then
-                        echo "Please enter $varname value in deploy.conf"
-                        validation='0'
-                fi
-        done
+	validation='1'
+	IFS=$'\n'
+	for field in $(echo $1 | sed 's/,/\n/g')
+	do
+		varname=$(echo $field | grep -o  ".*=>" | grep -o "\".*\"" | grep -o "[^\"]*")
+		value=$(echo $field | grep -o  "=>.*" | grep -o "\".*\"" | grep -o "[^\"]*" | sed 's/^\s*\(.*\)\s*$/\1/g')
+		if [ "$value" = "" ];then
+   			echo "Please enter $varname value in deploy.conf"
+			validation='0'
+   		fi
+ 	done 	  	
 
-        if [ $validation -eq "0" ]; then
-                exit
-        fi
+	if [ $validation -eq "0" ]; then
+		exit
+	fi
 }
 
 # get node by index
 node(){
-        node=$(($2+1))
-        echo $1 | sed 's/,/\n/g' | tail -n +$node | head -n 1 | grep -o "[^'\"]*"
+	node=$(($2+1))
+	echo $1 | sed 's/,/\n/g' | tail -n +$node | head -n 1 | grep -o "[^'\"]*"
 }
 
 # move node next to current and current to previous and previous to current and current to next 
 move(){
-        prevNode=$(node $list $(($current-1)))
-        currNode=$(node $list $current)
-        nextNode=$(node $list $(($current+1)))
-
-        if [ $1 = 'n' ];then
-                if [ -d $nextNode ] && [ ! -d $prevNode ];then
-                        mv $currNode $prevNode && mv $nextNode $currNode
-                        echo -e "Next site ${hl_path}$nextNode${nc} has been published to ${hl_path}$currNode${nc}"
-                elif [ ! -d $nextNode ];then
-                        echo "Next site is not there"
-                elif [ -d $prevNode ];then
-                        echo -e "Cannot move ${hl_path}$currNode${nc} to ${hl_path}$prevNode${nc} is already there"
-                fi
-        fi
-
-        if [ $1 = 'p' ];then
-                if [ -d $prevNode ] && [ ! -d $nextNode ];then
-                        mv $currNode $nextNode && mv $prevNode $currNode
-                        echo -e "Previous site ${hl_path}$prevNode${nc} has been restored to ${hl_path}$currNode${nc}"                  
-                elif [ ! -d $prevNode ];then
-                        echo "Previous site is not there"
-                elif [ -d $nextNode ];then
-                        echo -e "Cannot move ${hl_path}$currNode${nc} to ${hl_path}$nextNode${nc} is already there"
-                fi
-        fi
+	prevNode=$(node $list $(($current-1)))
+	currNode=$(node $list $current)
+	nextNode=$(node $list $(($current+1)))
+	
+	if [ $1 = 'n' ];then
+		if [ -d $nextNode ] && [ ! -d $prevNode ];then
+			mv $currNode $prevNode && mv $nextNode $currNode
+			echo -e "Next site ${hl_path}$nextNode${nc} has been published to ${hl_path}$currNode${nc}"
+		elif [ ! -d $nextNode ];then
+			echo "Next site is not there"
+		elif [ -d $prevNode ];then
+			echo -e "Cannot move ${hl_path}$currNode${nc} to ${hl_path}$prevNode${nc} is already there"
+		fi
+	fi
+	
+	if [ $1 = 'p' ];then
+		if [ -d $prevNode ] && [ ! -d $nextNode ];then
+			mv $currNode $nextNode && mv $prevNode $currNode
+			echo -e "Previous site ${hl_path}$prevNode${nc} has been restored to ${hl_path}$currNode${nc}"			
+		elif [ ! -d $prevNode ];then
+			echo "Previous site is not there"
+		elif [ -d $nextNode ];then
+			echo -e "Cannot move ${hl_path}$currNode${nc} to ${hl_path}$nextNode${nc} is already there"
+		fi
+	fi
 }
 
 # get database info from env.php
 env(){ 
-        cat $document_root/app/etc/env.php | grep ".*" | awk '{printf("%s ",$0)}' | grep -oP "'db'\s*=>(.(?!\]\s*\]\s*\]))*\s*.\s*.\s*." | grep -o  "'$1'\s*=>\s*'[^']*'\s*," | grep -o "'.*'" | grep -o "=>\s*'.*'" | grep -o "'.*'" | grep -o "[^']*"
+	cat $document_root/app/etc/env.php | grep ".*" | awk '{printf("%s ",$0)}' | grep -oP "'db'\s*=>(.(?!\]\s*\]\s*\]))*\s*.\s*.\s*." | grep -o  "'$1'\s*=>\s*'[^']*'\s*," | grep -o "'.*'" | grep -o "=>\s*'.*'" | grep -o "'.*'" | grep -o "[^']*"
 }
 
 # import current site db to next site db
 newdb(){
-        echo -e 'Import to new database '${hl_path}$dest_db${nc}
-        echo -e 'Current site '${hl_path}$source_db${nc}' password'
-        mysqldump --no-tablespaces --single-transaction -u $source_db_username -p $source_db > $document_root/var/$source_db.sql &&
+	echo -e 'Import to new database '${hl_path}$dest_db${nc}
+	echo -e 'Current site '${hl_path}$source_db${nc}' password'
+	mysqldump --no-tablespaces --single-transaction -u $source_db_username -p $source_db > $document_root/var/$source_db.sql &&
+	
+	echo -e 'New db '${hl_path}$dest_db${nc}' password'
+	mysql -u $dest_db_username -p $dest_db < $document_root/var/$source_db.sql
+	
+	rm $document_root/var/$source_db.sql
 
-        echo -e 'New db '${hl_path}$dest_db${nc}' password'
-        mysql -u $dest_db_username -p $dest_db < $document_root/var/$source_db.sql
-
-        rm $document_root/var/$source_db.sql
-
-        echo -e "Database has been transferred to ${hl_path}$dest_db${nc}"
+	echo -e "Database has been transferred to ${hl_path}$dest_db${nc}"
 }
 
 # deploy next site
 deploy(){
-        if [ -d $site_next ];then
-                rm -rf $site_next;
-        fi
-
-        mkdir $site_next && cd $site_next
-
-        git init
-        git remote add origin $git_repo
-        git pull origin $git_branch
-        git checkout $git_branch
-
-        # copy env.php and config.php to next site
-        cp $document_root/app/etc/env.php $document_root/app/etc/config.php $site_next/app/etc
-
-        # change database name in next site env.php
-        sed -i "s/'dbname'\s*=>\s*'.*'/'dbname' => '$dest_db'/g" $site_next/app/etc/env.php
-
-        read -p $(echo -e "Enter [y] to copy media from ${hl_path}$document_root/pub/media${nc} to ${hl_path}$site_next/pub/${nc}: ") input
-
+	if [ -d $site_next ];then
+		rm -rf $site_next;
+	fi
+ 
+	mkdir $site_next && cd $site_next
+	
+	git init
+	git remote add origin $git_repo
+	git pull origin $git_branch
+	git checkout $git_branch
+	
+	# copy env.php and config.php to next site
+	cp $document_root/app/etc/env.php $document_root/app/etc/config.php $site_next/app/etc
+	
+	# change database name in next site env.php
+	sed -i "s/'dbname'\s*=>\s*'.*'/'dbname' => '$dest_db'/g" $site_next/app/etc/env.php	
+	
+	read -p $(echo -e "Enter [y] to copy media from ${hl_path}$document_root/pub/media${nc} to ${hl_path}$site_next/pub/${nc}: ") input
+	
 	if [ "$input" = "y" ];then
-                echo -e "${hl_text}Copying media files....${nc} "
-                cp -R $document_root/pub/media  $site_next/pub/
+		echo -e "${hl_text}Copying media files....${nc} "
+		cp -R $document_root/pub/media  $site_next/pub/
 
-                echo -e "${hl_text}Media has been copied${nc}"
-        fi
-
-        $composer update
-
-        git reset --hard HEAD &&
-        git pull origin $git_branch
-
-        mage=$site_next'/bin/magento'
-
-        $php -dmemory_limit=-1  $mage setup:upgrade &&
-        $php -dmemory_limit=-1  $mage setup:di:compile &&
-        $php -dmemory_limit=-1  $mage setup:static-content:deploy -f $locales &&
-        $php -dmemory_limit=-1  $mage cache:flush
+		echo -e "${hl_text}Media has been copied${nc}"
+	fi
+	
+	$composer update
+	
+	git reset --hard HEAD &&
+	git pull origin $git_branch
+	
+	mage=$site_next'/bin/magento'
+	
+	$php -dmemory_limit=-1  $mage setup:upgrade &&
+	$php -dmemory_limit=-1  $mage setup:di:compile &&
+	$php -dmemory_limit=-1  $mage setup:static-content:deploy -f $locales &&
+	$php -dmemory_limit=-1  $mage cache:flush
 }
+
 # check if configuration file exists
 file
 
@@ -183,27 +184,28 @@ source_db_username=$(env "username")
 
 # database from current site will be imported to new site database
 read -p $(echo -e "Enter [y] to transfer data to new database ${hl_path}$dest_db${nc}: ") input
+
+
 if [ "$input" = "y" ];then
-        newdb
+	newdb
 fi
 
 # fetch from git and deploy in next site directory
 read -p $( echo -e "Enter [y] to deploy ${hl_path}$site_next${nc}: ") input
 
 if [ "$input" = "y" ];then
-        deploy
+	deploy
 fi
 
 # publish next site to current site or restore from previous site
 read -p $(echo -e  "Enter [y] to publish ${hl_path}$site_next${nc} to ${hl_path}$document_root${nc}: ") input
 
 if [ "$input" = "y" ];then
-        move "n"
+	move "n"
 fi
 
 read -p $(echo -e  "Enter [y] to restore ${hl_path}$site_prev${nc} to ${hl_path}$document_root${nc}: ") input
 
 if [ "$input" = "y" ];then
-        move "p"
+	move "p"
 fi
-
